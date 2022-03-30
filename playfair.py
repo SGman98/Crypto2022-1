fd0c25ced80f86602712dc67cbbd20a676666752
SGman98 <bastiangarnica@gmail.com>
2022-03-30 09:33:47 -0500

    Refine code structure and naming

--- playfair.py
+++ playfair.py
@@ -1,36 +1,39 @@
 
-def fix_text(text: str) -> str:
+def normalize(text: str) -> str:
     """
-    Fix the text implementing the playfair cipher rules.
+    Normalize the text implementing the rules of the playfair cipher.
 
     Remove spaces, replace j by i and remove spaces.
     If pairs of letters are the same, add an X in between.
     If length of the text is odd, add an X at the end.
 
     Parameters:
-        text (str): Text to be fixed.
+        text (str): Raw text.
 
     Returns:
-        str: Fixed text.
+        str: Normalized text.
 
     """
+    # Upper case, replace j by i and remove spaces
     text = text.upper().replace('J', 'I').replace(' ', '')
+
     i = 0
-    # Loop through the text
-    while i < len(text):
-        # If is the end of text and length is odd add X
-        if i + 1 >= len(text):
-            text += 'X'
-            break
-        # If the letters in the par is the same add X
+    # Loop through the text in pairs
+    while i < len(text) - 1:
+        # If the letters in the par are the same, add an X
         if text[i] == text[i + 1]:
             text = text[:i + 1] + 'X' + text[i + 1:]
-        # Increment i by 2 to iterate through the pairs
+
         i += 2
+
+    # If the length of the text is odd, add an X at the end
+    if len(text) % 2 == 1:
+        text += 'X'
+
     return text
 
 
-def create_key(key: str) -> list:
+def gen_matrix(key: str) -> list:
     """
     Create a 5x5 matrix from a key.
 
@@ -41,15 +44,21 @@ def create_key(key: str) -> list:
         list: Key matrix.
 
     """
+    # English alphabet without j
     alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
-    # Fix the key, replacing j by i and removing spaces
-    key = key.upper().replace('j', 'i').replace(' ', '')
-    # Add the key to the start of the alphabet
+
+    # Fix the key replacing j by i
+    key = key.upper().replace('J', 'I')
+
+    # Add the key and the alphabet in one string
     key += alphabet
-    # Filter letters that are not in the alphabet
+
+    # Filter letters in the key that are not in the alphabet
     key = ''.join(filter(lambda x: x in alphabet, key))
-    # Remove duplicates and keep the order
+
+    # Remove duplicates keeping the order of the letters
     key = ''.join(dict.fromkeys(key))
+
     # Create the key matrix
     key = [key[i:i + 5] for i in range(0, len(key), 5)]
     return key
@@ -60,7 +69,7 @@ def find_pos(letter: str, key: list) -> tuple:
     Find the position of a letter in the key matrix.
 
     Parameters:
-        letter (str): Letter to be found.
+        letter (str): Letter that we want to find.
         key (list): Key matrix.
 
     Returns:
@@ -69,6 +78,7 @@ def find_pos(letter: str, key: list) -> tuple:
     """
     # Transform the key to a string
     key = ''.join(key)
+
     # Find the position of the letter in the key
     row = key.find(letter) // 5
     col = key.find(letter) % 5
@@ -88,14 +98,21 @@ def playfair(text: str, key: str, mode: str) -> str:
         str: Encrypted or decrypted text.
 
     """
-    # Fix the text
-    text = fix_text(text)
-    # Create the key matrix
-    key = create_key(key)
+    # Make sure mode is 'e' or 'd'
+    if mode not in ['e', 'd']:
+        raise ValueError('Mode must be \'e\' or \'d\'')
+
+    # Normalize the text with the rules of the playfair cipher
+    text = normalize(text)
+
+    # Convert the key to a matrix
+    key = gen_matrix(key)
+
     # Transform the mode to 1 or -1
     mode = 1 if mode == 'e' else -1
 
     result = ''
+
     # Loop through the text in pairs
     for l1, l2 in zip(text[::2], text[1::2]):
         # Find the position of each letter
@@ -107,12 +124,17 @@ def playfair(text: str, key: str, mode: str) -> str:
             result += key[row1][(col1 + mode) % 5]
             result += key[row2][(col2 + mode) % 5]
             continue
+
         # If the letters are in the same column
         if col1 == col2:
             result += key[(row1 + mode) % 5][col1]
             result += key[(row2 + mode) % 5][col2]
             continue
+
         # If the letters are in different rows and columns
         result += key[row1][col2]
         result += key[row2][col1]
+
+    # Add spaces between the pairs
+    result = ' '.join(result[i:i + 2] for i in range(0, len(result), 2))
     return result
