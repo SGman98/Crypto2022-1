69490c0019712bfb28cd4502f01db2a3ce7dfb08
SGman98 <bastiangarnica@gmail.com>
2022-05-07 15:09:18 -0500

    Use os for paths

--- Talleres/DES/DES.py
+++ Talleres/DES/DES.py
@@ -1,5 +1,6 @@
 # Receive an image in any format
 import base64
+import os
 from pyDes import des, CBC, PAD_PKCS5
 
 
@@ -8,37 +9,38 @@ def des_cbc(key):
 
 
 def img_name(image_path):
-    image_path = image_path.split('/')[-1]
-    name, ext = image_path.split('.')
+    path, name = os.path.split(image_path)
+    name, ext = os.path.splitext(name)
     name = name.replace('_encrypted', '').replace('_decrypted', '')
-    return name, ext
+    return path, name, ext
 
 
 def encrypt(image_path, key):
-    name, ext = img_name(image_path)
-    with open(image_path, 'rb') as image:  # rb = read binary
-        image_bytes = image.read()
+    path, name, ext = img_name(image_path)
+    with open(image_path, 'rb') as img:
+        image_bytes = img.read()
         image_bytes_des = des_cbc(key).encrypt(image_bytes)
         image_bytes_base64 = base64.b64encode(image_bytes_des)
 
-        image_new_path = f'{name}_encrypted.{ext}'
-        with open(image_new_path, 'wb') as img_enc:
-            img_enc.write(image_bytes_base64)
-        return image_bytes_base64, image_new_path
+        enc_path = os.path.join(path, f'{name}_encrypted{ext}')
+        with open(enc_path, 'wb') as enc_img:
+            enc_img.write(image_bytes_base64)
+        return image_bytes_base64, enc_path
 
 
 def decrypt(encrypted_image, key):
-    name, ext = img_name(encrypted_image)
-    with open(encrypted_image, 'rb') as image:
-        image_bytes_base64 = image.read()
+    path, name, ext = img_name(encrypted_image)
+
+    with open(encrypted_image, 'rb') as img:
+        image_bytes_base64 = img.read()
         image_bytes = base64.b64decode(image_bytes_base64)
         image_bytes_des = des_cbc(key).decrypt(image_bytes)
 
-        image_new_path = f'{name}_decrypted.{ext}'
-        with open(image_new_path, 'wb') as img_dec:
-            img_dec.write(image_bytes_des)
+        dec_path = os.path.join(path, f'{name}_decrypted{ext}')
+        with open(dec_path, 'wb') as dec_img:
+            dec_img.write(image_bytes_des)
 
-        return image_bytes_des, image_new_path
+        return image_bytes_des, dec_path
 
 
 def main():
